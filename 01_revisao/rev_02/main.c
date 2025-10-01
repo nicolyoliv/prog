cd16d881d42195fc626572865fee18c1fc28f21b
nicolyoliv <nicolylopesoliv@gmail.com>
2025-10-01 11:54:30 -0300

    Atualiza README e adiciona main.c da revisão 01

--- 01_revisao/rev_02/main.c
+++ 01_revisao/rev_02/main.c
@@ -6,20 +6,13 @@ int main(){
     int n;
 int y=0;
     scanf("%d", &n);
-int x[n][n];
+
     for(int i=1; i<=n; i++){
-     
         for(int j=1; j<=i; j++){
-            
- 
                 y++;
                 printf("%d ", y);
-         
             }
-                   
  printf("\n");
     }
- 
-
     return 0;
 }
