7cf4687e3497d08378050d8cfd116065f2d7a3f9
nicolyoliv <nicolylopesoliv@gmail.com>
2025-10-12 12:01:41 -0300

    rec1

--- 02_recursao/rec_01/main.c
+++ 02_recursao/rec_01/main.c
@@ -1,22 +1,48 @@
 #include <stdio.h>
+#include <string.h>
+
 
 #define MAX 1000
 
 void ImprimeInvertido(char* string){
 
-printf("%s", )
+    int i, inicio = 0;
 
+    int tam = strlen(string);
+
+    if(tam > 0 && string[tam -1] == '\n'){
+        string[tam  - 1] = '\0';
+        tam --;
+    }
+
+    for(i = 0; i<= tam; i++){
+
+        if(string[i] == ' '|| string[i] == '\0'){
+
+        int fim = i-1;
+
+        for(int j = fim; j>= inicio; j--){
+            printf("%c", string[j]);
+        }
+        if(string[i] == ' '){
+                printf(" ");
+        }
+            inicio = i+1;
+        }
+
+    }
+     printf("\n");
 }
+
 
 
 int main(){
 
     char c[MAX];
-    int count = 0;
-    while(scanf("%s", &c[MAX]) && c!= '\0'){
-        count++;
+ 
+    if(fgets(c, MAX, stdin) != NULL){
+
+     ImprimeInvertido(c);
     }
-
-
     return 0;
 }
